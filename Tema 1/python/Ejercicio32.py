16961dd9e544174016c8586a4eb0457bb3217102
dcarjim0101 <dcarjim0101@g.educaand.es>
2025-10-14 13:35:21 +0200

    UD1

---
+++ Tema 1/python/Ejercicio32.py
@@ -0,0 +1,10 @@
+num = float(input("Introduce un número: "))
+numRedondeado = round (num)
+numABS = abs (numRedondeado)
+print (numABS)
+
+"""
+tambien se puede poner de esta forma:
+
+print (abs(round(float(input("introduce un numero: ")))))
+"""
