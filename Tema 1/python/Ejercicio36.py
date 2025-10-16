2ce59ea4d5135acffdb86fbf37d4951d22720728
dcarjim0101 <dcarjim0101@g.educaand.es>
2025-10-16 09:42:06 +0200

    UD1

--- Tema 1/python/Ejercicio36.py
+++ Tema 1/python/Ejercicio36.py
@@ -1,25 +1,32 @@
+import math
 def raiz_cuadrada(raiz):
-    raiz = nr * nr
+    raiz = n1 * n1
     return raiz
 
-opcion = int(input("elige una opcion, 1 para la suma, 2 para la resta, 3 para la multiplicacion, 4 para la division o 5 para la raiz cuadrada: "))
+opcion = int(input("elige una opcion, 1 para la suma, 2 para la resta, 3 para la multiplicacion, 4 para la division, 5 para la raiz cuadrada o 6 para la potencia: "))
 n1 = int(input("introduce un numero: "))
-n2 = int(input("introduce otro numero: "))
 
 match opcion:
         case 1:
+                n2 = int(input("introduce otro numero: "))
                 print (n1 + n2)
         case 2:
+                n2 = int(input("introduce otro numero: "))
                 print (n1 - n2)
         case 3:
+                n2 = int(input("introduce otro numero: "))
                 print (n1 * n2)
         case 4:
                 if n1 == 0 or n2 == 0:
                         print ("no se puede dividir entre 0")
                 else:
+                        n2 = int(input("introduce otro numero: "))
                         print (n1 / n2)
         case 5:
-                nr = int(input("introduce un numero: "))
                 print (raiz_cuadrada(opcion))
+        case 6:
+                n2 = int(input("introduce otro numero: "))
+                x = pow(n1, n2)
+                print(x)
         case _:
                 print("la opcion es incorrecta")
