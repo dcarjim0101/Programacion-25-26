8212494506ad1763abc8be7eacb7e4ee82ffdd9e
dcarjim0101 <dcarjim0101@g.educaand.es>
2025-12-03 14:35:26 +0100

    java UD 3

--- Tema 3/java/Ejercicios/src/prog/ud5/ejercicios/Ejercicio20.java
+++ Tema 3/java/Ejercicios/src/prog/ud5/ejercicios/Ejercicio20.java
@@ -3,26 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package prog.ud5.ejercicios;
-
 import java.util.Scanner;
-
 /**
  *
  * @author 02_1DAW_alum
  */
-public class Ejercicio5 {
+public class Ejercicio20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int nota1;
-        System.out.println("introduce una nota: ");
-        nota1 = sc.nextInt();
+        int nota;
+        nota = sc.nextInt();
         
-        int nota2;
-        System.out.println("introduce otra nota: ");
-        nota2 = sc.nextInt();
-        
-        int notafinal = nota1 + nota2;
-        float notamedia = notafinal / 2F;
-        System.out.println("la nota media es: " + notamedia);
+        if nota => 0 & nota<= 4 {
+            System.out.println("La nota es insuficiente");
+        elif nota => 5 & nota <= 6 {
+            System.out.println("la nota es un bien");
+        else
+        }
+        }
     }
 }
