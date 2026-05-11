48a7c638078412502c47525a336dd9710a87c069
dcarjim0101 <dcarjim0101@g.educaand.es>
2026-05-11 10:56:17 +0200

    UD9

--- Tema 9/src/ejercicio7/Ejercicio7.java
+++ Tema 9/src/ejercicio7/Ejercicio7.java
@@ -4,9 +4,15 @@ import java.io.PrintStream;
 
 public class Ejercicio7 {
 	public static void main(String[] args) {
+		
 		PrintStream salida = System.out;
 
-        salida.println("Hola mundo");
+        salida.println("salida 1");
         salida.println(123);
+        
+        PrintStream salida2 = System.out;
+
+        salida2.println("salida 2");
+        salida2.println(123);
 	}
 }
